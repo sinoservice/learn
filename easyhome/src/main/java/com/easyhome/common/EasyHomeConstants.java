8b75fa05b6f32a3a3a67810e065cc47dd461b33c
小菜鸟 <50493888+13330184187@users.noreply.github.com>
2019-05-11 19:21:40 +0800

    Update EasyHomeConstants.java

--- easyhome/src/main/java/com/easyhome/common/EasyHomeConstants.java
+++ easyhome/src/main/java/com/easyhome/common/EasyHomeConstants.java
@@ -7,7 +7,7 @@ package com.easyhome.common;
  */
 public class EasyHomeConstants {
 	
-	// 接口处理结果123
+	// 接口处理结果1234
 	public static final String INTERFACE_EXECUTE_RESULT_SUCCESS = "0"; //成功123
 	public static final String INTERFACE_EXECUTE_RESULT_FAIL = "-1"; //成功111
 }
