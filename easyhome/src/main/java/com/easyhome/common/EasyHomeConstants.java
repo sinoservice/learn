40d4532a8467fd8e5a66d4844ba0da49ceb22807
13330184187 <767098629@qq.com>
2019-05-11 19:16:44 +0800

    1

--- easyhome/src/main/java/com/easyhome/common/EasyHomeConstants.java
+++ easyhome/src/main/java/com/easyhome/common/EasyHomeConstants.java
@@ -7,7 +7,7 @@ package com.easyhome.common;
  */
 public class EasyHomeConstants {
 	
-	// 接口处理结果12
+	// 接口处理结果125
 	public static final String INTERFACE_EXECUTE_RESULT_SUCCESS = "0"; //成功123
 	public static final String INTERFACE_EXECUTE_RESULT_FAIL = "-1"; //成功111
 }
